cdfc3ac7328440f502d17851d91b472011bc58b4
abedin-ashraf <nurulabedinashraf@gmail.com>
2024-03-01 10:24:11 -0600

    Initialize DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,53 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+//Initializing the Prisma Client
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+// Create the main Hono app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  }
+}>();
+
+app.post('/api/v1/signup', (c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+
+  return c.text('signup route')
 })
 
-export default app
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+  return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id')
+  console.log(id);
+  return c.text('get blog route')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+
+  return c.text('get blog bulk')
+})
+
+
+
+export default app;
+
+
